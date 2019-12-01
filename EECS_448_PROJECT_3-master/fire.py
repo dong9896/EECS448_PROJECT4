9ef193b71232f4e37186676f8de533a4357c38d9
jialei zhou <47931522+AAlexZhou@users.noreply.github.com>
2019-12-01 17:38:05 -0600

    Add files via upload

--- EECS_448_PROJECT_3-master/fire.py
+++ EECS_448_PROJECT_3-master/fire.py
@@ -4,6 +4,14 @@ class bulltes():
 
     imgpath3 = 'image/bomb.png'
     def __init__(self,master,x,y):
+        """
+       Constructs bulltes for ship. Keep creating bulltes in window
+       :param: master
+       :param: x location for the master
+       :param: y location for the master
+       :return: returns none.
+       :pre: After player starting game
+       """
         self._master= master
         self.x =x
         self.y =y
@@ -12,16 +20,38 @@ class bulltes():
         self.image2= pygame.image.load(self.imgpath3)
         self.lives =1
     def  update(self):
+        """
+        Give bullets a moving direction
+        :return: returns none.
+        :pre: after construct generate a location for bullets
+        """
         self.y += self.speed
         if self.y >= 800:
             self.on =False
 
     def draw(self):
+        """
+              Draw the shape of bullets
+              :return: returns none.
+              :pre: after construct generate a location for bullets
+              :post: draw shape on that location
+              """
         self._master.blit(self.image2, (self.x, self.y))
 
     def get_center(self):
+        """
+             Help those check function to know whether shark touch bullets
+             :return: returns the center coordinate of that bullets.
+             :pre: There is bullets in the list
+             :post: Help those check function to know whether shark touch bullets
+             """
 
         return self.x + radius / 2, self.y + radius / 2
     def get_radius(self):
-
+        """
+              Help those check function to know whether shark touch bullets
+              :return: returns the center coordinate of that bullets.
+              :pre: There is bullets in the list
+              :post: Help those check function to know whether shark touch bullets
+              """
         return radius / 2
--- EECS_448_PROJECT_3-master/ship.py
+++ EECS_448_PROJECT_3-master/ship.py
@@ -6,6 +6,12 @@ class Ship:
     imgpath = 'image/ship.png'
     imgpath2 = 'image/ship2.png'
     def __init__(self, master):
+        """
+                Constructs ship. Keep creating food in window
+                :param: master
+                :return: returns none.
+                :pre: After player starting game
+                """
         self._master=master
         self.image= pygame.image.load(self.imgpath)
         self.x= 200
@@ -15,6 +21,12 @@ class Ship:
         self.t = 0
         self.bullte =[]
     def move(self):
+        """
+                update the coordinate after starting move ship
+                :return: returns none.
+                :pre: After player starting move their shark
+                :post: update coordinate ship
+                """
         if self.x == 0-65 and self.bool == False:
             self.update()
             self.x += self.speed
@@ -26,9 +38,18 @@ class Ship:
         elif self.bool == False:
             self.x -= self.speed
     def draw(self):
+        """
+                Renders the img of shark(For project 3, black circle stands for shark)
+                """
         self._master.blit(self.image, (self.x, self.y))
 
     def update(self):
+        """
+                Help those check function to know whether ship touch bullets
+                :return: returns the center coordinate of that ship.
+                :pre: There is ship exist in window
+                :post: Help those check function to know whether ship touch bullets
+                """
         if self.bool == True:
             self.image = pygame.image.load(self.imgpath2)
             self.bool = False
@@ -38,6 +59,11 @@ class Ship:
 
 
     def fire(self):
+        """
+                Generate coordinate for bullets and the time gap
+                :return: returns none.
+                :post: Generate coordinate for bullets and the time gap
+                """
         cd = random.randint(1, 10)
         self.t += cd
         if self.t >= self.firecd:
@@ -48,6 +74,12 @@ class Ship:
             self.bullte.append(Bullte)
 
     def update_b(self):
+        """
+              update the list of bullets
+              :return: returns none.
+              :pre: There is settled bullets in the window
+              :post: update the list of bullets
+              """
         temp=[]
         for b  in self.bullte:
             b.update()
@@ -56,10 +88,16 @@ class Ship:
         self.bullte = temp
 
     def draw_b(self):
+        """
+                Draw food on window
+                """
         for b in self.bullte:
             b.draw()
 
     def clean(self):
+        """
+                       Clean food on window
+                       """
         self.x =200
         self.y =50
         self.bullte =[]
